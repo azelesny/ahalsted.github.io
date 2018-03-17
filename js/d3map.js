3866c71ce9d3429f51bc18044916d29b66c94939
Alicea Zelesny <alicea.zelesny@gmail.com>
2018-03-17 14:18:12 -0400

    Almost done with tutorial

--- js/d3map.js
+++ js/d3map.js
@@ -1,9 +1,22 @@
 //wrapping everything in self executing anonymous function to move to local scope
 (function(){
+
 //pseudo-global variables
 var asianArray = ["Human_Dev_Rank", "Human_Dev_Index", "GNI_per_capita", "POV_DAY", "POV_DAY_YEAR", "POV_NATIONAL", "POV_NATIONAL_YEAR", "OUT_OF_SCHOOL", "OUT_FEMALE_PRIMARY", "MALNUTRITION_UNDERWEIGHT", "MALNUTRITION_YEAR", "MALNUTRITION_WORLDBANK_2015"];
 var expressed = asianArray[0]; //initial attributes
-
+//chart frame dimensions
+var chartWidth = window.innerWidth*0.425,
+    chartHeight = 473;
+    leftPadding = 25,
+    rightPadding = 2,
+    topBottomPadding = 5,
+    chartInnerWidth = chartWidth - leftPadding - rightPadding,
+    chartInnerHeight = chartHeight - topBottomPadding * 2,
+    translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+//create a scale to size bars proportionally to frame
+var yScale = d3.scaleLinear()
+      .range([463, 0])
+      .domain([0, 200]);
 //execute script when window is loaded
 window.onload = setMap();
 
@@ -57,6 +70,7 @@ function setMap(){
       setEnumerationUnits(asianCountries, map, path, colorScale);
       //add coordinated visualization to the map
       setChart(csvData, colorScale);
+      createDropdown(csvData);
         };//end of function callback
 };//end of setMap
 
@@ -98,7 +112,15 @@ function setEnumerationUnits(asianCountries, map, path, colorScale){
         .attr("d", path)
         .style("fill", function(d){
             return choropleth(d.properties, colorScale);
+        })
+        .on("mouseover", function(d){
+          highlight(d.properties);
+        })
+        .on("mouseout", function(d){
+            dehighlight(d.properties);
         });
+    var desc = asian.append("desc")
+            .text('{"stroke": "#000", "stroke-width": "0.5px"}');
 };//end setEnumerationUnits
 
 //function to create color scale generator
@@ -138,15 +160,6 @@ function choropleth(props, colorScale){
 
 //function to create coordinated bar chart
 function setChart(csvData, colorScale){
-    //chart frame dimensions
-    var chartWidth = window.innerWidth*0.425,
-        chartHeight = 473;
-        leftPadding = 25,
-        rightPadding = 2,
-        topBottomPadding = 5,
-        chartInnerWidth = chartWidth - leftPadding - rightPadding,
-        chartInnerHeight = chartHeight - topBottomPadding * 2,
-        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
     //create a second svg element to hold the bar chart
     var chart = d3.select("body")
         .append("svg")
@@ -159,10 +172,6 @@ function setChart(csvData, colorScale){
         .attr("width", chartInnerWidth)
         .attr("height", chartInnerHeight)
         .attr("transform", translate);
-    //create a scale to size bars proportionally to frame
-    var yScale = d3.scaleLinear()
-          .range([463, 0])
-          .domain([0, 200]);
     //set bars for each province
     var bars = chart.selectAll(".bars")
         .data(csvData)
@@ -174,33 +183,11 @@ function setChart(csvData, colorScale){
         .attr("class", function(d){
             return "bars " + d.ADM0_A3;
         })
-        .attr("width", chartWidth / csvData.length - 1)
-        .attr("x", function(d, i){
-            return i * (chartWidth / csvData.length)+leftPadding;
-        })
-        .attr("height", function(d){
-          //make sure attribute value is a number
-          var val = parseFloat(d[expressed]);
-          //if attribute value exists, assign a color; otherwise assign gray
-          if (typeof val == 'number' && !isNaN(val)){
-              return 463 - yScale(parseFloat(d[expressed]));
-          } else {
-              return 0;
-          };
-        })
-        .attr("y", function(d){
-          //make sure attribute value is a number
-          var val = parseFloat(d[expressed]);
-          //if attribute value exists, assign a color; otherwise assign gray
-          if (typeof val == 'number' && !isNaN(val)){
-              return yScale(parseFloat(d[expressed])) + topBottomPadding;
-          } else {
-              return 0;
-          };
-        })
-        .style("fill", function(d){
-            return choropleth(d, colorScale);
-        });
+        .attr("width", chartWidth / csvData.length-1)
+        .on("mouseover", highlight)
+        .on("mouseout", dehighlight);
+  var desc = bars.append("desc")
+          .text('{"stroke": "none", "stroke-width": "0px"}');
     // //annotate bars with attribute value text
     //   var numbers = chart.selectAll(".numbers")
     //       .data(csvData)
@@ -250,6 +237,121 @@ function setChart(csvData, colorScale){
               .attr("y", 40)
               .attr("class", "chartTitle")
               .text("Variable " + expressed[0] + " in each nation");
+    updateChart(bars, csvData.length,colorScale);
 };//end of setChart
+
+//function to create a dropdown menu for attribute selection
+function createDropdown(csvData){
+    //add select element
+    var dropdown = d3.select("body")
+        .append("select")
+        .attr("class", "dropdown")
+        .on("change", function(){
+                    changeAttribute(this.value, csvData)
+                });
+    //add initial option
+    var titleOption = dropdown.append("option")
+        .attr("class", "titleOption")
+        .attr("disabled", "true")
+        .text("Select Attribute");
+
+    //add attribute name options
+    var attrOptions = dropdown.selectAll("attrOptions")
+        .data(asianArray)
+        .enter()
+        .append("option")
+        .attr("value", function(d){ return d })
+        .text(function(d){ return d });
+};//end of createDropdown
+
+//dropdown change listener handler
+function changeAttribute(attribute, csvData){
+    //change the expressed attribute
+    expressed = attribute;
+    //recreate the color scale
+    var colorScale = makeColorScale(csvData);
+    //recolor enumeration units
+    var asian = d3.selectAll(".countries")
+        .transition()
+        .duration(1000)
+        .style("fill", function(d){
+            return choropleth(d.properties, colorScale)
+        });
+    //re-sort, resize, and recolor bars
+    var bars = d3.selectAll(".bars")
+        //re-sort bars
+        .sort(function(a, b){
+            return b[expressed] - a[expressed];
+        })
+        .transition() //add animation
+        .delay(function(d,i){
+          return i*20
+        })
+        .duration(500);
+      updateChart(bars, csvData.length,colorScale);
+};//end of changeAttribute
+
+//function to position, size, and color bars in chart
+function updateChart(bars, n, colorScale){
+    //position bars
+    bars.attr("x", function(d, i){
+            return i * (chartInnerWidth / n) + leftPadding;
+        })
+        //resize bars
+        .attr("height", function(d, i){
+          //make sure attribute value is a number
+          var val = parseFloat(d[expressed]);
+          //if attribute value exists, assign a color; otherwise assign gray
+          if (typeof val == 'number' && !isNaN(val)){
+              return 463 - yScale(parseFloat(d[expressed]));
+          } else {
+              return 0;
+          };
+        })
+        .attr("y", function(d, i){
+          //make sure attribute value is a number
+          var val = parseFloat(d[expressed]);
+          //if attribute value exists, assign a color; otherwise assign gray
+          if (typeof val == 'number' && !isNaN(val)){
+              return yScale(parseFloat(d[expressed])) + topBottomPadding;
+          } else {
+              return 0;
+          };
+        })
+        //color/recolor bars
+        .style("fill", function(d){
+            return choropleth(d, colorScale);
+        });
+    var chartTitle = d3.select(".chartTitle")
+        .text("Variable " + expressed[0] + " in each nation");
+};
+//function to highlight enumeration units and bars
+function highlight(props){
+    //change stroke
+    var selected = d3.selectAll("." + props.ADM0_A3)
+        .style("stroke", "blue")
+        .style("stroke-width", "2");
+};
+
+//function to reset the element style on mouseout
+function dehighlight(props){
+    var selected = d3.selectAll("." + props.ADM0_A3)
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
+        });
+
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
+
+        var styleObject = JSON.parse(styleText);
+
+        return styleObject[styleName];
+    };
+};
 
 })();//last line of d3map.js
