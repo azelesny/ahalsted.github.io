83992f4ab25230391289bf82af77a83f3813a927
Alicea Zelesny <alicea.zelesny@gmail.com>
2018-06-11 18:00:06 -0400

    New Portfolio Page

--- js/main.js
+++ js/main.js
@@ -1,16 +1,1 @@
 //main javascript file
-var source = $('#txt').attr('data-text');
-var dest = $('#textHolder');
-
-function typeWriter(text, n) {
-    if (n < (text.length)) {
-        dest.html(text.substring(0, n+1));
-        n++;
-        setTimeout(function() {
-            typeWriter(text, n)
-        }, 100);
-    }
-}
-
-//call the function when HTML doc is ready
-$(document).ready(typeWriter(source, 0));
